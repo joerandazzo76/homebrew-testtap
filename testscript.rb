b274a430c20536a6519f711a4b0f18933534d945
Giuseppe Randazzo <joerandazzo76@gmail.com>
2022-08-06 19:04:02 -0400

    add formula

---
+++ testscript.rb
@@ -0,0 +1,12 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/joerandazzo76/testscript/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "283b2a6f1266bcb1a22d53c918797a630112e5c0261e7d2dec0218f834545f39"
+
+
+  def install
+	bin.install "testscript"
+  end
+
+end
