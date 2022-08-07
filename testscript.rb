cb380fdc78aa02c1ac35b3fdec56ab98c20ef701
Giuseppe Randazzo <joerandazzo76@gmail.com>
2022-08-06 20:06:10 -0400

    added brew script

--- testscript.rb
+++ testscript.rb
@@ -4,9 +4,8 @@ class Testscript < Formula
   url "https://github.com/joerandazzo76/testscript/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "283b2a6f1266bcb1a22d53c918797a630112e5c0261e7d2dec0218f834545f39"
 
-
   def install
-	bin.install "testscript"
+    bin.install "testscript"
+    bin.install "brew"
   end
-
 end
